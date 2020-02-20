78e77d9d4d6aae739032375d1ac9abb31fbb5f7f
devmaxim <avex.52@mail.ru>
2020-02-20 21:10:31 +0300

    commit

---
+++ js/main.js
@@ -0,0 +1,119 @@
+$(function(){
+
+	$('.main__button-1, .assortment__button-2, .header__icon-media, .header__call-action, .change__button, .calculation__button, .footer__tel-button').click(function(){
+		$('.modal').show();
+	});
+	$('.modal__close').click(function(){
+		$('.modal').hide();
+	});
+	$('.thanks__close').click(function(){
+		$('.thanks').toggleClass('thanks__show');
+	});
+
+	$('.backlight__slider').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		vertical: true,
+		verticalSwiping: true,
+		arrow: true,
+		prevArrow: '<div class="backlight-arrow backlight-arrow_up"></div>',
+		nextArrow: '<div class="backlight-arrow backlight-arrow_down"></div>',
+		responsive: [	
+			{		   
+			    breakpoint: 767,
+			    settings:  {
+			    	vertical: false,
+			    	verticalSwiping: false
+			    }
+		    }
+		]	   
+	})
+
+	$('.completed__slider').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		vertical: true,
+		verticalSwiping: true,
+		arrow: true,
+		prevArrow: '<div class="backlight-arrow backlight-arrow_up completed-arrow_up"></div>',
+		nextArrow: '<div class="backlight-arrow backlight-arrow_down completed-arrow_down"></div>',
+		responsive: [	
+			{		   
+			    breakpoint: 992,
+			    settings:  {
+			    	vertical: false,
+			    	verticalSwiping: false
+			    }
+		    }
+		]	   
+	})
+
+	$('.cooperation__slider').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		vertical: true,
+		verticalSwiping: true,
+		arrow: true,
+		prevArrow: '<div class="backlight-arrow backlight-arrow_up cooperation-arrow_up"></div>',
+		nextArrow: '<div class="backlight-arrow backlight-arrow_down cooperation-arrow_down"></div>'	   
+	})
+
+	jQuery(function($){
+	   $(".catalog__form-input, .communication__form-tel, .discount__form-tel, .cooperation__form-tel, .modal__form-tel").mask("+7 (999) 999-99-99");
+	});
+
+
+	$('.cooperation__form, .communication__form').submit(function(event) {
+		event.preventDefault();
+		$.ajax({
+			type: "POST",
+			url: "mailer/consultation.php",
+			data: $(this).serialize()
+		}).done(function() {
+			$(this).find("input").val("");
+			$("form").trigger("reset");
+			$('.thanks').toggleClass('thanks__show');
+		});
+		return false;
+	});
+	$('.modal__form').submit(function(event) {
+		event.preventDefault();
+		$.ajax({
+			type: "POST",
+			url: "mailer/smart.php",
+			data: $(this).serialize()
+		}).done(function() {
+			$(this).find("input").val("");
+			$("form").trigger("reset");
+			$('.modal').hide();
+			$('.thanks').toggleClass('thanks__show');
+		});
+		return false;
+	});
+	$('.discount__form').submit(function(event) {
+		event.preventDefault();
+		$.ajax({
+			type: "POST",
+			url: "mailer/discount.php",
+			data: $(this).serialize()
+		}).done(function() {
+			$(this).find("input").val("");
+			$("form").trigger("reset");
+			$('.thanks').toggleClass('thanks__show');
+		});
+		return false;
+	});
+	$('.catalog__form').submit(function(event) {
+		event.preventDefault();
+		$.ajax({
+			type: "POST",
+			url: "mailer/catalog.php",
+			data: $(this).serialize()
+		}).done(function() {
+			$(this).find("input").val("");
+			$("form").trigger("reset");
+			$('.thanks').toggleClass('thanks__show');
+		});
+		return false;
+	});
+})
