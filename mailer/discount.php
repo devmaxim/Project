3059b73ff1e4e06a63db35ee5007112a765d5490
devmaxim <avex.52@mail.ru>
2020-02-27 17:57:51 +0300

    edits

--- mailer/discount.php
+++ mailer/discount.php
@@ -12,13 +12,13 @@ $mail->CharSet = 'utf-8';
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.mail.ru';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'Логин';                 		// Наш логин от ящика
-$mail->Password = 'Пароль';                           // Наш пароль от ящика от которого будут приходить письма
+$mail->Username = 'beautiful.stairs@mail.ru';                 		// Наш логин от ящика
+$mail->Password = 'A85M94fbF1';                           // Наш пароль от ящика от которого будут приходить письма
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
  
-$mail->setFrom('Почта', 'От кого');   // От кого будут приходить письма
-$mail->addAddress('Почта');     // Куда будут приходить письма
+$mail->setFrom('beautiful.stairs@mail.ru', 'Красивые лестницы');   // От кого будут приходить письма
+$mail->addAddress('project@ad-market.site');     // Куда будут приходить письма
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
